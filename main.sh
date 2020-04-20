2d7526c0a38cdede6e81ff08e14230c30ba5b2a3
binghesam <bing_he@foxmail.com>
2020-04-20 15:27:59 -0400

    bash command for running the deteciton

--- main.sh
+++ main.sh
@@ -1,1 +1,14 @@
 #! /bin/bash
+
+# bash for running the detection model
+python detection/train.py \
+--train ./../data/biased.full.train \
+--test ./../data/biased.full.test \
+--learning_rate 0.0003 \
+--epochs 1 \
+--hidden_size 512 \
+--train_batch_size 32 \
+--test_batch_size 16 \
+--debias_weight 1.3
+
+# generate the json after processing
