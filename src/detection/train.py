40135d8fc6ffffa52d73bceeda4d5d0046507d64
binghesam <bing_he@foxmail.com>
2020-04-21 00:05:01 -0400

    tok_probs only

--- src/detection/train.py
+++ src/detection/train.py
@@ -96,7 +96,7 @@ print('EVAL...')
 model.eval()
 results = utils.run_inference(model, eval_dataloader, loss_fn, tokenizer)
 ## capture results
-json_string = json.dumps({"tok_logits": results["tok_logits"]})
+json_string = json.dumps({"tok_probs": results["tok_probs"]})
 with open("final_result.json",'w') as f:
     f.write(json_string)
 ## added lines to capture the results
