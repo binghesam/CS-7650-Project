a589f6665dfa0d6f4728ee562aaed25aa57fb745
Richard Ding <richard.ding.gt@gmail.com>
2020-04-21 18:24:59 -0400

    change data main.sh datapath

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@
 
 # bash for running the detection model
 python ./src/detection/train.py \
---train ./../data/biased.full.train \
---test ./../data/biased.full.test \
+--train ./data/biased.full.train \
+--test ./data/biased.full.test \
 --learning_rate 0.0003 \
 --epochs 1 \
 --hidden_size 512 \
