5819c07c0e040365e38fcd801375c8d20d335832
Richard Ding <richard.ding.gt@gmail.com>
2020-04-22 16:36:32 -0400

    update main.sh

--- main.sh
+++ main.sh
@@ -11,21 +11,21 @@
 # --test_batch_size 16 \
 # --debias_weight 1.3
 
-python ./src/detection/train.py \
- --train ./data/biased.full.filtered.train \
- --test ./data/biased.full.filtered.test \
- --extra_features_top --pre_enrich --activation_hidden \
- --learning_rate 0.0003 \
- --epochs 6 \
- --hidden_size 512 \
- --train_batch_size 32 \
- --test_batch_size 16 \
- --debias_weight 1.3
+# python ./src/detection/train.py \
+#  --train ./data/biased.full.filtered.train \
+#  --test ./data/biased.full.filtered.test \
+#  --extra_features_top --pre_enrich --activation_hidden \
+#  --learning_rate 0.0003 \
+#  --epochs 6 \
+#  --hidden_size 512 \
+#  --train_batch_size 32 \
+#  --test_batch_size 16 \
+#  --debias_weight 1.3
 
 python ./src/detection/train.py \
  --train ./data/biased.full.filtered.train \
  --test ./data/biased.full.filtered.test \
---categories_file $DATA/revision_topics.csv \
+--categories_file ./src/revision_topics.csv \
  --extra_features_top --pre_enrich --activation_hidden --category_input \
   --learning_rate 0.0003 --epochs 20 --hidden_size 512 --train_batch_size 32 \
    --test_batch_size 16 --debias_weight 1.3 
