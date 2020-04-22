b3a6cae47a8de70f1c08ddf2f85abeafb505b489
Richard Ding <richard.ding.gt@gmail.com>
2020-04-22 17:03:02 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -30,7 +30,7 @@
 #   --learning_rate 0.0003 --epochs 20 --hidden_size 512 --train_batch_size 32 \
 #    --test_batch_size 16 --debias_weight 1.3 --working_dir train_tagging/
 
-python ./src/detection/diyi/neutralizing-bias-master/src/train.py \
+python ./src/detection/diyi/neutralizing-bias-master/src/tagging/train.py \
 --train ./data/biased.full.filtered.train \
 --test ./data/biased.full.filtered.test\
 --categories_file ./data/revision_topics.csv \
