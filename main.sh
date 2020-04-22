0cee1c6fe3adad8121facea7a86f323bae4a47c1
Richard Ding <richard.ding.gt@gmail.com>
2020-04-22 16:38:50 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -25,7 +25,7 @@
 python ./src/detection/train.py \
  --train ./data/biased.full.filtered.train \
  --test ./data/biased.full.filtered.test \
---categories_file ./src/revision_topics.csv \
+--categories_file ./src/data/revision_topics.csv \
  --extra_features_top --pre_enrich --activation_hidden --category_input \
   --learning_rate 0.0003 --epochs 20 --hidden_size 512 --train_batch_size 32 \
    --test_batch_size 16 --debias_weight 1.3 
