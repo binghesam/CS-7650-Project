20e1e4b04011ad7b68026afde49031db8e9df389
Richard Ding <richard.ding.gt@gmail.com>
2020-04-22 01:04:46 -0400

    update readme

--- download_data_ckpk.sh
+++ download_data_ckpk.sh
@@ -1,4 +1,4 @@
-wget http://nlp.stanford.edu/projects/bias/bias_data.zip
-unzip bias_data.zip
+# wget http://nlp.stanford.edu/projects/bias/bias_data.zip
+# unzip bias_data.zip
 
-wget https://nlp.stanford.edu/projects/bias/model.ckpt
+# wget https://nlp.stanford.edu/projects/bias/model.ckpt
