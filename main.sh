d8c111276e912f54dcd95b1f8d50625047c0796e
Richard Ding <richard.ding.gt@gmail.com>
2020-04-22 17:33:11 -0400

    update main

--- main.sh
+++ main.sh
@@ -30,13 +30,16 @@
 #   --learning_rate 0.0003 --epochs 20 --hidden_size 512 --train_batch_size 32 \
 #    --test_batch_size 16 --debias_weight 1.3 --working_dir train_tagging/
 
-python ./src/detection/diyi/neutralizing-bias-master/src/tagging/train.py \
---train ./data/biased.full.filtered.train \
---test ./data/biased.full.filtered.test\
---categories_file ./data/revision_topics.csv \
---extra_features_top --pre_enrich --activation_hidden --category_input \
---learning_rate 0.0003 --epochs 6 --hidden_size 512 --train_batch_size 32 \
---test_batch_size 16 --debias_weight 1.3 --working_dir train_tagging/
+# python ./src/detection/diyi/neutralizing-bias-master/src/tagging/train.py \
+# --train ./data/biased.full.filtered.train \
+# --test ./data/biased.full.filtered.test\
+# --categories_file ./data/revision_topics.csv \
+# --extra_features_top --pre_enrich --activation_hidden --category_input \
+# --learning_rate 0.0003 --epochs 6 --hidden_size 512 --train_batch_size 32 \
+# --test_batch_size 16 --debias_weight 1.3 --working_dir train_tagging/
+
+cd /src/detection/diyi2/neutralizing-bias2April13/src
+python tagging/train.py --train ./data/biased.full.filtered.train --test ./data/biased.full.filtered.test --categories_file ./data/revision_topics.csv --extra_features_top --pre_enrich --activation_hidden --category_input --learning_rate 0.0003 --epochs 6 --hidden_size 512 --train_batch_size 32 --test_batch_size 16 --debias_weight 1.3 --working_dir train_tagging/
 
 # generate the json after processing
 
