a3dc687681ca235de38b9f8d26264d3f12a56140
Richard Ding <richard.ding.gt@gmail.com>
2020-04-22 21:57:50 -0400

    Update seq2seq_translate_pipeline.py

--- src/seq2seq/seq2seq_translate_pipeline.py
+++ src/seq2seq/seq2seq_translate_pipeline.py
@@ -529,7 +529,7 @@ def evaluateRandomlyClustering(encoder, decoder, n=10):
     for i in range(n):
         pair = random.choice(pairs_clustering)
         print('>', pair[0])
-        print('=', pair[1])
+        # print('=', pair[1])
         output_words, attentions = evaluate(encoder, decoder, pair[0])
         output_sentence = ' '.join(output_words)
         print('<', output_sentence)
