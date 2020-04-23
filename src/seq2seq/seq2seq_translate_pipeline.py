b81995c97d62c84ab5fb13a3f421be60d765b9b8
Richard Ding <richard.ding.gt@gmail.com>
2020-04-22 20:51:35 -0400

    Update seq2seq_translate_pipeline.py

--- src/seq2seq/seq2seq_translate_pipeline.py
+++ src/seq2seq/seq2seq_translate_pipeline.py
@@ -140,6 +140,8 @@ def prepareData(lang1, lang2, reverse=False):
     for pair in pairs:
         input_lang.addSentence(pair[0])
         output_lang.addSentence(pair[1])
+    for pair in pairs_clustering:
+        input_lang.addSentence(pair[0])
     print("Counted words:")
     print(input_lang.name, input_lang.n_words)
     print(output_lang.name, output_lang.n_words)
