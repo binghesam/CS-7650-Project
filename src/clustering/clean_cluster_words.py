14a0473c79bc7eca4bdfe0a58318084b426ac806
Richard Ding <richard.ding.gt@gmail.com>
2020-04-22 21:52:28 -0400

    change

--- src/clustering/clean_cluster_words.py
+++ src/clustering/clean_cluster_words.py
@@ -8,10 +8,11 @@ with open('./cluster_words.txt') as f:
         line_cleaned = re.sub('##','',line_cleaned)
         biased_phrase.append(line_cleaned)
 
+
 with open('./bias-unbias.space.only','w') as f:
     text = ''
     for line in biased_phrase:
-    	if len(line.rstrip()) == 0:
-    		line = 'dummy'
+    	# if len(line.rstrip()) == 0:
+    	# 	line = 'dummy'
         text+=line.rstrip()+'\t'+' '+'\n'
     f.write(text)
